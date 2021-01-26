454eb1db84b39e22c1eb739e6dc5c1f2bba34f62
AlexeyPashkevich <gamevirus2015@gmail.com>
2021-01-26 05:18:40 +0200

    added new items

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require('gulp'),
   cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss')
+  return gulp.src('app/scss/style.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(autoprefixer({
@@ -20,10 +20,11 @@ gulp.task('sass', function () {
 
 gulp.task('style', function () {
   return gulp.src([
-    'node_modules/normalize.css/normalize.css', 
+    'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/rateyo/src/jquery.rateyo.css',
+    'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -36,6 +37,7 @@ gulp.task('script', function () {
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -61,9 +63,9 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
-    gulp.watch('app/*.html', gulp.parallel('html'))
-    gulp.watch('app/js/*.js', gulp.parallel('js'))
+  gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+  gulp.watch('app/*.html', gulp.parallel('html'))
+  gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
-gulp.task('default', gulp.parallel('style','script', 'sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
