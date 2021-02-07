052e0d459ddaf272bfedd59dac9d0f019e730036
AlexeyPashkevich <gamevirus2015@gmail.com>
2021-02-07 07:22:10 +0200

    adaptive header and footer, fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -33,6 +33,16 @@ $(function(){
     $('.icon-th-list').removeClass('active'); 
   });
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
+  
+  
+
   $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function (event) {
     var id = $(this).attr('data-id');
     $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
